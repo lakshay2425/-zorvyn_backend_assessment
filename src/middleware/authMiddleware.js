cd047b40114dd810e7115d5b4ae6c170aa73604d
Lakshay <lakshay12290@gmail.com>
2026-04-03 14:22:54 +0530

    feat: add authMiddleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,52 @@
+import { config } from "../config/config.js"
+import createHttpError from 'http-errors'
+import jwt from "jsonwebtoken";
+import { returnResponse } from "../utilis/returnResponse.js";
+
+const jwtSecret = config.get("JWT_SECRET");
+const environment = config.get("NODE_ENVIRONMENT");
+
+
+const verifyAuthStatus = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+
+        if (!token) {
+            return returnResponse("No Token is provided", res, 400);
+        }
+
+        // Verify token
+        const decoded = jwt.verify(token, jwtSecret, {
+            algorithms: ["HS256"]
+        });
+
+        if (!decoded || typeof decoded === "string" || !('userInfo' in decoded)) return next(createHttpError(400, "You're unauthorized to access this resource"));
+
+        // Add user info to request object
+        req.user = {
+            gmail: decoded.userInfo.userEmail
+        };
+
+        next();
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            return next(createHttpError(401, "Token has expired, please login again"));
+        }
+        if (error instanceof jwt.JsonWebTokenError) {
+            return next(createHttpError(401, "Invalid token, please login again"));
+        }
+        console.error("Auth middleware error:", error.message);
+        return next(createHttpError(500, "Internal server error"));
+    }
+};
+
+
+export const optionalAuth = async (req, res, next) => {
+    if (environment === "development" && config.get("BYPASS_AUTH") === 'true') {
+        req.user = {
+            gmail: config.get("TEST_USER_EMAIL")
+        }
+        return next();
+    }
+    return verifyAuthStatus(req, res, next);
+}

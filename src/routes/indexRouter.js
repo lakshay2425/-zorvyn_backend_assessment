f346f9e10d9ab94d462de770207a4414c44bc0d9
Lakshay <lakshay12290@gmail.com>
2026-04-04 01:01:53 +0530

    feat: register all routes in indexRoute file

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import userRouter from './user.js';
-// import analyticsRouter from './analytics.js';
-// import transactionRouter from './transaction.js';
+import { optionalAuth } from '../middleware/authMiddleware.js';
+import analyticsRouter from './analytics.js';
+import transactionRouter from './transaction.js';
 
 router.use("/users", userRouter);
-// router.use("/analytics", analyticsRouter);
-// router.use("/transaction", transactionRouter);
+router.use("/analytics", optionalAuth, analyticsRouter);
+router.use("/transactions", optionalAuth, transactionRouter);
 
 export default router;

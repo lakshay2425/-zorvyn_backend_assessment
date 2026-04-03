8f217e7d2c9f69f375e0800cbce92f43917fe5c9
Lakshay <lakshay12290@gmail.com>
2026-04-04 02:19:39 +0530

    feat: add cookieParser package

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from 'express';
 import globalErrorHandler from './src/middleware/globalErrorHandler.js';
 import indexRouter from './src/routes/indexRouter.js';
 import { connectToDatabase } from './src/config/mongoose.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connectToDatabase();

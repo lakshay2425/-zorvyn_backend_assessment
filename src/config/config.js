22def5b9eb42eb8ce624a78c8d13578774e19571
Lakshay <lakshay12290@gmail.com>
2026-04-03 17:01:16 +0530

    refactor: remove optionalValue from config.js file

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,18 @@
 const _config = {
     PORT: process.env.PORT,
-    NODE_ENVIRONMENT: process.env.NODE_ENV || 'development',
-    dbURI: process.env.DB_URI || 'mongodb://localhost:27017/base',
-    JWT_SECRET: process.env.JWT_SECRET || "your-default-secret-key",
-    BYPASS_AUTH: process.env.BYPASS_AUTH || "false",
-    TEST_USER_EMAIL: process.env.TEST_USER_EMAIL || "your-test-email",
-    DOMAIN: process.env.DOMAIN || "localhost"
+    NODE_ENVIRONMENT: process.env.NODE_ENV,
+    dbURI: process.env.DB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    BYPASS_AUTH: process.env.BYPASS_AUTH,
+    TEST_USER_EMAIL: process.env.TEST_USER_EMAIL,
+    DOMAIN: process.env.DOMAIN
 }
 
 
 export const config = {
     get(key) { 
         const value = _config[key];
-        if (value === undefined || value === null) {
+        if (value === undefined || value === null || value === "") {
             console.error(`Config key "${key}" not found.`);
             process.exit(1);
         }
